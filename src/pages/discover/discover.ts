e2b9c4eb7d72a1e17d8077f44b400691def27577
MorentDennis <dennis.morent@student.howest.be>
2018-04-25 11:35:49 +0200

    cleanup

--- src/pages/discover/discover.ts
+++ src/pages/discover/discover.ts
@@ -1,15 +1,42 @@
-import { Component, ViewChild  } from '@angular/core';
-import {  NavController, NavParams, LoadingController, ModalController, Events } from 'ionic-angular';
-import { MovieProvider } from '../../providers/movie/movie';
-import { Platform } from 'ionic-angular/platform/platform';
-import { HomePage } from '../home/home';
-import { Content } from 'ionic-angular';
-import { SuperTabs } from 'ionic2-super-tabs';
-import { FormControl } from '@angular/forms';
+import {
+  Component,
+  ViewChild
+} from '@angular/core';
+import {
+  NavController,
+  NavParams,
+  LoadingController,
+  ModalController,
+  Events
+} from 'ionic-angular';
+import {
+  MovieProvider
+} from '../../providers/movie/movie';
+import {
+  Platform
+} from 'ionic-angular/platform/platform';
+import {
+  HomePage
+} from '../home/home';
+import {
+  Content
+} from 'ionic-angular';
+import {
+  SuperTabs
+} from 'ionic2-super-tabs';
+import {
+  FormControl
+} from '@angular/forms';
 import 'rxjs/add/operator/debounceTime';
-import { MovieDetailPage } from '../movie-detail/movie-detail';
-import { ImageLoaderConfig } from 'ionic-image-loader';
-import { LoginPage } from '../login/login';
+import {
+  MovieDetailPage
+} from '../movie-detail/movie-detail';
+import {
+  ImageLoaderConfig
+} from 'ionic-image-loader';
+import {
+  LoginPage
+} from '../login/login';
 
 
 @Component({
@@ -26,7 +53,7 @@ export class DiscoverPage {
 
   searchTerm: string = '';
   searchControl: FormControl;
-  
+
   movieDetailPage = MovieDetailPage;
   loginPage = LoginPage
   movies: any;
@@ -41,65 +68,51 @@ export class DiscoverPage {
     private imageLoaderConfig: ImageLoaderConfig,
     public modalCtrl: ModalController,
     public events: Events
-    ) {
-
-      this.searchControl = new FormControl();
+  ) 
+  {
+    this.searchControl = new FormControl();
+    this.getMoviesInTheatre();
     
-      
-      //imageLoaderConfig.enableSpinner(true);
-      
-      
-      
   }
 
   login() {
     let loginModal = this.modalCtrl.create(this.loginPage);
     loginModal.present();
   }
-
-
   ionViewDidLoad() {
-    
-    
- this.events.subscribe("discover:updated", (searchTerm) => {
-     this.setMoviesByKeyWords_async(searchTerm);
-   })}
+    this.events.subscribe("discover:updated", (searchTerm) => {
+      this.setMoviesByKeyWords_async(searchTerm);
+    })
+  }
 
-   
-   
-   
-  
- 
-    
-    
-    
+  async getMoviesInTheatre() {
+    this.movies = await this.movieProvider.getMoviesInTheater();
+  }
 
-  ionViewDidEnter() {
-     
-    }
+  ionViewDidEnter() {}
 
-    openMovieDetail(i) {
-      let movie = this.movies[i];
-      this.navCtrl.push(this.movieDetailPage, {movie: movie}
-      );
-    }
+  openMovieDetail(i) {
+    let movie = this.movies[i];
+    this.navCtrl.push(this.movieDetailPage, {
+      movie: movie
+    });
+  }
 
-
-    async setMoviesByKeyWords_async(keyword)
-    {
-      try {
-        let loading = this.loadingCtrl.create({cssClass: 'transparent'});
+  async setMoviesByKeyWords_async(keyword) {
+    try {
+      let loading = this.loadingCtrl.create({
+        cssClass: 'transparent'
+      });
       loading.present();
       const keywords = await this.movieProvider.getKeyWords(keyword);
       const movies = await this.movieProvider.getRelatedMovies(keywords)
       this.movies = movies;
       loading.dismiss();
-      }
-      catch(err) {
-        console.log("error in setting the movies by keyword : " + err);
-      }
-
-      
+    } catch (err) {
+      console.log("error in setting the movies by keyword : " + err);
     }
+
+
+  }
 
 }
